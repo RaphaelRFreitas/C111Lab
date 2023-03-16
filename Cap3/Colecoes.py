212926be3564b3193f27430079abc67c4a0022c4
RaphaelRFreitas <raphaelrrfreitas@gmail.com>
2023-03-15 22:21:30 -0300

    Final Aula 15/03

--- Cap3/Colecoes.py
+++ Cap3/Colecoes.py
@@ -19,15 +19,6 @@ lista.pop(1) #Delete2
 corte = lista[1:] #Slice
 print(f'Lista nova {lista} e pedaco {corte}')
 
-#Dicionarios
-#Dicionarios sao colecoes que nao sao ordenadas, mutaveis e indexadas
-dicionario = {"nome": "Goku", "idade": 40, "habilidade": "Kamehameha"}
-print(f'Dicionario {dicionario}')
-
-dicionario["nome"] = "Kuririn" #Update
-dicionario.pop("idade") #Delete
-print(f'Dicionario novo {dicionario}')
-
 #Conjuntos
 #Conjuntos sao colecoes que nao sao ordenadas e nao permitem valores duplicados
 conjunto = {"Goku", "Vegeta", "Trunks", "Gohan"}
@@ -35,3 +26,11 @@ print(f'Conjunto {conjunto}')
 conjunto.add("Piccolo") #Create
 conjunto.remove("Gohan") #Delete
 print(f'Conjunto novo {conjunto}')
+
+#Dicionarios
+#Dicionarios sao colecoes que nao sao ordenadas, mutaveis e indexadas
+dicionario = {"nome": "Goku", "idade": 40, "habilidade": "Kamehameha"}
+print(f'Dicionario {dicionario}')
+dicionario["nome"] = "Kuririn" #Update
+dicionario.pop("idade") #Delete
+print(f'Dicionario novo {dicionario}')
